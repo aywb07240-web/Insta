2df2d555734e0780a9d39f11bcfb38bcad0171b4
bdhrhrurheh3-max <bdhrhrurheh3@gmail.com>
2025-11-16 00:18:31 +0430

    testing

---
+++ testing_passwords.py
@@ -0,0 +1,37 @@
+# Hi if you seeing this comment i have to say: this is tool made and its not real one but if you want real one send a massege to this number +93782059574
+print ('hi welcome! here you can test passwords ! on instagram time to start !')
+B = '''
+       _____ _____   ____             _       
+     |_   _/ ____| |  _ \            | |     NASA RED TEAM 
+        | || |  __  | |_) |_ __ _   _| |_ ___ 
+        | || | |_ | |  _ <| '__| | | | __/ _ |
+       _| || |__| | | |_) | |  | |_| | ||  __/
+      |_____\_____| |____/|_|   \__,_|\__\___|
+     NOte: We don't Accept any responsibility for any illegal usage.                                          
+               Made by nasa team ! '''
+print(B) 
+user = input('plese enter your username :   ')
+for i in range(1,10000):
+    print (f'your {user} is under scanning.. ')
+    print (f'your {user} is under scanning.... ')
+for b in range(1,10000):
+    print (f'your {user} is under scanning.. ')
+    print (f'your {user} is under scanning.... ')
+for c in range(1,20000):
+    print(f'testing : {user} > [10101010101]')
+    print(f'testing : {user} > [hajcbjdhbds]')
+    print(f'testing : {user} > [djrhruvir9b]')
+    print(f'testing : {user} > [20202020202]')
+    print(f'testing : {user} > [30303030300]')
+for n in range(1,10000):
+    print('finshing.')
+    print('finshing..')
+    print('finshing...')
+    print('finshing....')
+    print('finshing.....')
+result = """ found ! /afghanesta1123/ """
+print(f'key found for user {user}: afghanesta1123')
+print(result)
+
+
+
